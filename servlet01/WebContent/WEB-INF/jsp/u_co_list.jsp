9888d6f1931015730df077a36ca67d33319bfd2a
shun9q <s.hatakeyama.sys21@morijyobi.ac.jp>
2022-07-14 14:27:25 +0900

    u_co_list/javaの追加

---
+++ servlet01/WebContent/WEB-INF/jsp/u_co_list.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
+
+<jsp:include page="template/template_top.jsp"/>
+
+<div class="ui container" style="min-height: 75vh;">
+<br><br><br><br>
+	<div class="ui three column grid">
+
+	</div>
+</div>
+
+<jsp:include page="template/template_bottom.jsp"/>
---
+++ servlet01/src/model/u_co_list.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/u_co_list")
+public class u_co_list extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher =
+				request.getRequestDispatcher("WEB-INF/jsp/user/u_co_list.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}

93b6f8dab37f2c8ad2aac6c3673ada2558eb32df
Seniorious15 <m.noazaka.sys21@morijyobi.ac.jp>
2022-07-20 11:33:09 +0900

    a_co_modify_confirm

--- servlet01/src/servlet_admin/a_co_list.java
+++ servlet01/src/servlet_admin/a_co_list.java
@@ -8,6 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import scopedata.Account;
 
 
 @WebServlet("/a_co_list")
@@ -17,6 +20,9 @@ public class a_co_list extends HttpServlet {
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Account account = new Account("管理者","taiyu.o","小笠原太優");
+		HttpSession session = request.getSession();
+		session.setAttribute("account",account);
 		RequestDispatcher dispatcher =
 				request.getRequestDispatcher("WEB-INF/jsp/admin/a_co_list.jsp");
 		dispatcher.forward(request, response);
